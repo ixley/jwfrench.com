6438f7b1509397edc1e3dabd4073926a515cdbc5
Jeff French <jfrench@ixley.com>
2011-04-22 09:40:18 -0700

    launch

--- compass/config.rb
+++ compass/config.rb
@@ -3,7 +3,7 @@
 http_path = "../"
 css_dir = "../css"
 sass_dir = "src"
-images_dir = "images"
-javascripts_dir = "javascripts"
+images_dir = "../images"
+javascripts_dir = "../js"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
