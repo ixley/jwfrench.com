bd1f2912c0713d987a56a048f274bc8964a8d5c0
Jeff French <jfrench@ixley.com>
2011-05-16 17:08:46 -0700

    add resume

--- compass/config.rb
+++ compass/config.rb
@@ -5,5 +5,8 @@ css_dir = "../css"
 sass_dir = "src"
 images_dir = "../images"
 javascripts_dir = "../js"
+output_style = :compressed
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
+
+environment = :development
