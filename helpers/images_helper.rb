7f417d617b75bc23b2f86d3bbcdb6a6ea3cb4599
Jeff French <jfrench@ixley.com>
2015-11-25 13:22:42 -0800

    WIP aspect ratio for images_helper

--- helpers/images_helper.rb
+++ helpers/images_helper.rb
@@ -6,6 +6,14 @@ module ImagesHelper
   # 
   # => <img src="/assets/ants_1980.jpg" srcset="/assets/pic_640.jpg 640w, /assets/pic_1024.jpg 1024w, /assets/pic_1980.jpg 1980w" sizes="100vw" class="my-image">
   #
+
+
+  # TODO: trying to get ratio to set size for srcset helper...
+  def aspect_ratio(source)
+    size = FastImage.size("#{source}")
+    ratio = size[0]/size[1].to_f
+  end
+
   def image_set_tag(source, srcset = {}, options = {})
     srcset = srcset.map { |src, size| "#{image_path(src)} #{size}" }.join(', ')
     image_tag(source, options.merge(srcset: srcset))
