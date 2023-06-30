ea9a4e24976adb3991d64307a92696d26aa19a6c
Jeff French <jfrench@ixley.com>
2023-06-30 12:41:34 -0400

    stop tracking public directory

--- public/js/headroom-snippet.js
+++
@@ -1,8 +0,0 @@
-// if you're using a bundler, first import:
-import Headroom from "headroom.js";
-// grab an element
-var myElement = document.querySelector("site-header__container");
-// construct an instance of Headroom, passing the element
-var headroom = new Headroom(myElement);
-// initialise
-headroom.init();
